a7cba9915b90fe491da81c7fb18b91c5da4d8c38
NhanVu9053 <nhanvu9053@gmail.com>
2020-07-20 15:12:07 +0700

    sdads

---
+++ SearchAndSortDemo/Array.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchAndSortDemo
+{
+    public class Array
+    {
+        public int[] CreatArray(int num)
+        {
+            int[] arr = new int[num];
+            Random rnd = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                arr[i] = rnd.Next(30, 60);
+            }
+            return arr;
+        }
+        public void SortArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+
+                    }
+                }
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+
+        }
+        public void ShowArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public bool CheckSymetricalArray(int[] arr)
+        {
+            bool check = true;
+            int n = arr.Length / 2;
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (arr[i] != arr[n - i - 1])
+                {
+                    return check = false;
+                }
+            }
+            return check;
+        }
+        public int find(int[] arr, int n)
+        {
+            int low = 0; int hight = arr.Length - 1; 
+            while (hight > low)
+            {
+                int mid = arr.Length / 2;
+                if (n < arr[mid])
+                {
+                    low = mid - 1;
+                }
+                else if (n == arr[mid])
+                {
+                    return mid;
+                    
+                }
+                else
+                {
+                    hight = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+    }
+
+}
---
+++ SearchAndSortDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SearchAndSortDemo
+{
+    class Program
+    {
+        private static int[] arr;
+        static void Main(string[] args)
+        {
+            Array array = new Array();
+            //Console.WriteLine("input N:");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr = array.CreatArray(n);
+            //array.ShowArray(arr);
+            //Console.WriteLine("kiem tra mang doi xung");
+            //Console.WriteLine(array.CheckSymetricalArray(arr));
+            //array.SortArray(arr);
+            //Console.WriteLine($"array after sort:");
+            //array.ShowArray(arr);
+            int choice = -1;
+            while (choice != 5)
+            {
+                Console.WriteLine("Please input an option:");
+                Console.WriteLine("1. Create array. ");
+                Console.WriteLine("2. Check for symmetric arrays.");
+                Console.WriteLine("3. Array sort."); ;
+                Console.WriteLine("4. Find.");
+                Console.WriteLine("5. Exit.");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("input N:");
+                        int n = int.Parse(Console.ReadLine());
+                        arr = array.CreatArray(n);
+                        array.ShowArray(arr);
+                        break;
+                    case 2:
+                        Console.WriteLine("Check Symetrical Array :");
+                        Console.WriteLine(array.CheckSymetricalArray(arr));
+                        break;
+                    case 3:
+                        array.SortArray(arr);
+                        Console.WriteLine($"array after sort:");
+                        array.ShowArray(arr);
+                        break;
+                    case 4:
+                        Console.WriteLine($"input value ");
+                        int value = int.Parse(Console.ReadLine());
+                     int check =   array.find(arr,value);
+                        if(check !=-1)
+                        {
+                            Console.WriteLine($"{value} nam o vi tri {check}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("not found");
+                        }
+                        break;
+                   
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
